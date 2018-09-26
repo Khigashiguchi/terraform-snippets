201133310b825bc313438cd1d5028f63c6dd8f26
Kazuki Higashiguchi <k.higashiguchi@binc.jp>
2018-09-26 18:11:48 +0900

    fixed by terraform fmt:

--- simple-demo/main.tf
+++ simple-demo/main.tf
@@ -1,14 +1,14 @@
 provider "aws" {
   access_key = "${var.aws_access_key}"
   secret_key = "${var.aws_secret_key}"
-  region = "${var.aws_region}"
+  region     = "${var.aws_region}"
 }
 
 resource "aws_instance" "example" {
-  ami = "ami-08847abae18baa040" // Amazon Linux 2 AMI (HVM), SSD Volume Type
+  ami           = "ami-08847abae18baa040" // Amazon Linux 2 AMI (HVM), SSD Volume Type
   instance_type = "t2.micro"
 
-  tags{
+  tags {
     Name = "第130回PHP勉強会"
   }
 }
