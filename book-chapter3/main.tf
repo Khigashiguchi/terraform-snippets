c210f1974a175293a622b6fac4b9cab436dcdac1
hgsgtk <hgsgtk@gmail.com>
2019-07-18 12:33:20 +0900

    add tag, region, profile

--- book-chapter3/main.tf
+++ book-chapter3/main.tf
@@ -1,4 +1,23 @@
+provider "aws" {
+  region  = "ap-northeast-1"
+  profile = "study-terraform"
+}
+
+variable "example_instance_type" {
+  default = "t3.micro"
+}
+
 resource "aws_instance" "example" {
-    ami = "ami-0f9ae750e8274075b"
-    instance_type = "t3.micro"
+  ami           = "ami-0f9ae750e8274075b"
+  instance_type = var.example_instance_type
+
+  tags = {
+    Name = "chapter3"
+  }
+
+  user_data = <<EOF
+        #!/bin/bash
+        yum install -y httpd
+        systemctl start httpd.service
+EOF
 }
