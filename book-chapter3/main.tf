02dafdc9ae28e279f7dfce00d820519ad27d5417
hgsgtk <hgsgtk@gmail.com>
2019-08-02 17:40:10 +0900

    3.4.1 Interpolation Syntax

--- book-chapter3/main.tf
+++ book-chapter3/main.tf
@@ -3,6 +3,24 @@ provider "aws" {
   profile = "study-terraform"
 }
 
+resource "aws_security_group" "example_ec2" {
+  name = "example_ec2"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 data "aws_ami" "recent_amazon_linux_2" {
   most_recent = true
   owners      = ["amazon"]
@@ -19,10 +37,17 @@ data "aws_ami" "recent_amazon_linux_2" {
 }
 
 resource "aws_instance" "example" {
-  ami           = data.aws_ami.recent_amazon_linux_2.image_id
-  instance_type = "t3.micro"
+  ami                    = data.aws_ami.recent_amazon_linux_2.image_id
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.example_ec2.id]
+
+  user_data = <<EOF
+#!/bin/bash
+yum install -y httpd
+systemctl start httpd.service
+EOF
 }
 
 output "example_instance_id" {
-  value = aws_instance.example.id
+  value = aws_instance.example.public_dns
 }
