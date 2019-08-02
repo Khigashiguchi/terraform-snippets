f04522bcebda4693a472e883da1905a8cefde309
hgsgtk <hgsgtk@gmail.com>
2019-08-02 17:21:52 +0900

    3-2にて実験用に2台目を投入

--- book-chapter3/main.tf
+++ book-chapter3/main.tf
@@ -21,3 +21,23 @@ resource "aws_instance" "example" {
         systemctl start httpd.service
 EOF
 }
+
+resource "aws_instance" "example1" {
+  ami = "ami-0f9ae750e8274075b"
+  instance_type = "t3.nano"
+
+  network_interface {
+    network_interface_id = "eni-0a8242d191c1126ee"
+    device_index = 0
+  }
+
+  tags = {
+    Name = "chapter3-1"
+  }
+
+  user_data = <<EOF
+        #!/bin/bash
+        yum install -y httpd
+        systemctl start httpd.service
+EOF
+}
