6faa213e3fac2a89d48bafd96dc328ec253fe708
hgsgtk <hgsgtk@gmail.com>
2019-08-02 17:27:19 +0900

    3.3.3 出力値

--- book-chapter3/main.tf
+++ book-chapter3/main.tf
@@ -3,41 +3,11 @@ provider "aws" {
   profile = "study-terraform"
 }
 
-variable "example_instance_type" {
-  default = "t3.micro"
-}
-
 resource "aws_instance" "example" {
   ami           = "ami-0f9ae750e8274075b"
-  instance_type = var.example_instance_type
-
-  tags = {
-    Name = "chapter3"
-  }
-
-  user_data = <<EOF
-        #!/bin/bash
-        yum install -y httpd
-        systemctl start httpd.service
-EOF
+  instance_type = "t3.micro"
 }
 
-resource "aws_instance" "example1" {
-  ami = "ami-0f9ae750e8274075b"
-  instance_type = "t3.nano"
-
-  network_interface {
-    network_interface_id = "eni-0a8242d191c1126ee"
-    device_index = 0
-  }
-
-  tags = {
-    Name = "chapter3-1"
-  }
-
-  user_data = <<EOF
-        #!/bin/bash
-        yum install -y httpd
-        systemctl start httpd.service
-EOF
+output "example_instance_id" {
+  value = aws_instance.example.id
 }
