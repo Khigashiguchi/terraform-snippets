409669e5c04b8cb7e42703b3b73bb4352bdf3b73
hgsgtk <hgsgtk@gmail.com>
2019-08-15 15:27:29 +0900

    IAMロールをモジュール化して利用

--- programatic-terraform/containered/main.tf
+++ programatic-terraform/containered/main.tf
@@ -3,6 +3,13 @@ provider "aws" {
   profile = "study-terraform"
 }
 
+module "describe_regions_for_ec2" {
+  source = "./iam_role"
+  name = "describe-regions-for-ec2"
+  identifier = "ec2.amazonaws.com"
+  policy = data.aws_iam_policy_document.allow_describe_regions.json
+}
+
 data "aws_iam_policy_document" "allow_describe_regions" {
   statement {
     effect = "Allow"
@@ -10,30 +17,4 @@ data "aws_iam_policy_document" "allow_describe_regions" {
     # リージョン一覧を取得する
     resources = ["*"]
   }
-}
-
-resource "aws_iam_policy" "example" {
-  name = "example"
-  policy = data.aws_iam_policy_document.allow_describe_regions.json
-}
-
-data "aws_iam_policy_document" "ec2_assume_role" {
-  statement {
-    actions = ["sts:AssumeRole"]
-
-    principals {
-      identifiers = ["ec2.amazonaws.com"]
-      type = "Service"
-    }
-  }
-}
-
-resource "aws_iam_role" "example" {
-  name = "example"
-  assume_role_policy = data.aws_iam_policy_document.ec2_assume_role.json
-}
-
-resource "aws_iam_role_policy_attachment" "example" {
-  role = aws_iam_role.example.name
-  policy_arn = aws_iam_policy.example.arn
 }
