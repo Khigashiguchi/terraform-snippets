42a11b64e36b172192b2b6f93b7ae75574ab605d
hgsgtk <hgsgtk@gmail.com>
2019-08-17 13:38:19 +0900

    chapter 7 network

--- programatic-terraform/containered/main.tf
+++ programatic-terraform/containered/main.tf
@@ -1,22 +1,22 @@
 provider "aws" {
-  region = "ap-northeast-1"
+  region  = "ap-northeast-1"
   profile = "study-terraform"
-}
-
-module "describe_regions_for_ec2" {
-  source = "./iam_role"
-  name = "describe-regions-for-ec2"
-  identifier = "ec2.amazonaws.com"
-  policy = data.aws_iam_policy_document.allow_describe_regions.json
 }
 
 data "aws_iam_policy_document" "allow_describe_regions" {
   statement {
-    effect = "Allow"
+    effect  = "Allow"
     actions = ["ec2:DescribeRegions"]
     # リージョン一覧を取得する
     resources = ["*"]
   }
+}
+
+module "describe_regions_for_ec2" {
+  source     = "./iam_role"
+  name       = "describe-regions-for-ec2"
+  identifier = "ec2.amazonaws.com"
+  policy     = data.aws_iam_policy_document.allow_describe_regions.json
 }
 
 resource "aws_s3_bucket" "private" {
@@ -41,16 +41,16 @@ resource "aws_s3_bucket" "private" {
 }
 
 resource "aws_s3_bucket_public_access_block" "private" {
-  bucket = aws_s3_bucket.private.id
-  block_public_acls = true
-  block_public_policy =  true
-  ignore_public_acls = true
+  bucket                  = aws_s3_bucket.private.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
   restrict_public_buckets = true
 }
 
 resource "aws_s3_bucket" "public" {
   bucket = "public-pragmatic-terraform-on-aws-hgsgtk001"
-  acl = "public-read"
+  acl    = "public-read"
 
   # CORS: Cross-Origin Resource Sharing
   cors_rule {
@@ -80,12 +80,12 @@ resource "aws_s3_bucket_policy" "alb_log" {
 
 data "aws_iam_policy_document" "alb_log" {
   statement {
-    effect = "Allow"
-    actions = ["s3:PutObject"]
+    effect    = "Allow"
+    actions   = ["s3:PutObject"]
     resources = ["arn:aws:s3:::${aws_s3_bucket.alb_log.id}/*"]
 
     principals {
-      type = "AWS"
+      type        = "AWS"
       identifiers = ["582318560864"]
     }
   }
@@ -106,13 +106,20 @@ resource "aws_vpc" "example" {
   }
 }
 
-resource "aws_subnet" "public" {
+resource "aws_subnet" "public_0" {
   vpc_id = aws_vpc.example.id
   # /24単位できる
-  cidr_block = "10.0.0.0/24"
+  cidr_block = "10.0.1.0/24"
   # サブネットで起動したインスタンスに自動でIPを付与
   map_public_ip_on_launch = true
-  availability_zone = "ap-northeast-1a"
+  availability_zone       = "ap-northeast-1a"
+}
+
+resource "aws_subnet" "public_1" {
+  vpc_id                  = aws_vpc.example.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "ap-northeast-1c"
+  map_public_ip_on_launch = true
 }
 
 resource "aws_internet_gateway" "example" {
@@ -126,14 +133,104 @@ resource "aws_route_table" "public" {
 # ルートテーブルの1レコードに該当
 # VPC以外への通信をインターネットゲートウェイ経由でインターネットへ流す
 resource "aws_route" "public" {
-  route_table_id = aws_route_table.public.id
-  gateway_id = aws_internet_gateway.example.id
-  destination_cidr_block =  "0.0.0.0/0"
+  route_table_id         = aws_route_table.public.id
+  gateway_id             = aws_internet_gateway.example.id
+  destination_cidr_block = "0.0.0.0/0"
 }
 
 # どのルートテーブルを使ってルーティングするかをサブネット単位で判断
 # ルートテーブルとサブネットを関連付け
-resource "aws_route_table_association" "public" {
-  subnet_id = aws_subnet.public.id
+resource "aws_route_table_association" "public_0" {
+  subnet_id      = aws_subnet.public_0.id
   route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public_1" {
+  subnet_id      = aws_subnet.public_1.id
+  route_table_id = aws_route_table.public.id
+}
+
+# private networkの作成
+resource "aws_subnet" "private_0" {
+  vpc_id                  = aws_vpc.example.id
+  cidr_block              = "10.0.65.0/24"
+  availability_zone       = "ap-northeast-1a"
+  map_public_ip_on_launch = false
+}
+
+resource "aws_subnet" "private_1" {
+  vpc_id                  = aws_vpc.example.id
+  cidr_block              = "10.0.66.0/24"
+  availability_zone       = "ap-northeast-1c"
+  map_public_ip_on_launch = false
+}
+
+resource "aws_route_table" "private_0" {
+  vpc_id = aws_vpc.example.id
+}
+
+resource "aws_route_table" "private_1" {
+  vpc_id = aws_vpc.example.id
+}
+
+# defaultでNAT Gatewayにrouting
+resource "aws_route" "private_0" {
+  route_table_id         = aws_route_table.private_0.id
+  nat_gateway_id         = aws_nat_gateway.nat_gateway_0.id
+  destination_cidr_block = "0.0.0.0/0"
+}
+
+resource "aws_route" "private_1" {
+  route_table_id         = aws_route_table.private_1.id
+  nat_gateway_id         = aws_nat_gateway.nat_gateway_1.id
+  destination_cidr_block = "0.0.0.0/0"
+}
+
+resource "aws_route_table_association" "private_0" {
+  subnet_id      = aws_subnet.private_0.id
+  route_table_id = aws_route_table.private_0.id
+}
+
+resource "aws_route_table_association" "private_1" {
+  subnet_id      = aws_subnet.private_1.id
+  route_table_id = aws_route_table.private_1.id
+}
+
+# NAT Network Address Translation
+resource "aws_eip" "nat_gateway_0" {
+  # EIP in VPC
+  # See also https://www.terraform.io/docs/providers/aws/r/eip.html#vpc
+  vpc = true
+  # EIP may require IGW to exist prior to association. Use depends_on to set an explicit dependency on the IGW.
+  # See also https://www.terraform.io/docs/providers/aws/r/eip.html
+  depends_on = [aws_internet_gateway.example]
+}
+
+resource "aws_eip" "nat_gateway_1" {
+  vpc        = true
+  depends_on = [aws_internet_gateway.example]
+}
+
+# NAT gatewayをpublic subnetに配置
+resource "aws_nat_gateway" "nat_gateway_0" {
+  allocation_id = aws_eip.nat_gateway_0.id
+  subnet_id     = aws_subnet.public_0.id
+  # 依存先作成後にこのリソースが作成されることを保証する
+  depends_on = [aws_internet_gateway.example]
+}
+
+resource "aws_nat_gateway" "nat_gateway_1" {
+  allocation_id = aws_eip.nat_gateway_1.id
+  subnet_id     = aws_subnet.public_1.id
+  depends_on    = [aws_internet_gateway.example]
+}
+
+# Firewall
+# AWSではsubnetレベルの Network ACL・インスタンスレベルの Security Groupがある
+module "example_sg" {
+  source      = "./security_group"
+  name        = "module-sg"
+  vpc_id      = aws_vpc.example.id
+  port        = 80
+  cidr_blocks = ["0.0.0.0/0"]
 }
