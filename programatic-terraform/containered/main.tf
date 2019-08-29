934b6acf7588a5277873a23eae0cdaf76d6de290
hgsgtk <hgsgtk@gmail.com>
2019-08-29 16:50:10 +0900

    9.2 ECS Cluster, Service, Task の作成

--- programatic-terraform/containered/main.tf
+++ programatic-terraform/containered/main.tf
@@ -470,3 +470,69 @@ resource "aws_lb_listener_rule" "example" {
     values = ["/*"]
   }
 }
+
+# ECS
+# Cluster
+resource "aws_ecs_cluster" "example" {
+  name = "example"
+}
+
+# Task Definition
+# Task = コンテナ実行単位
+resource "aws_ecs_task_definition" "example" {
+  # タスク定義名のPrefix
+  # これにリビジョン番号を付与したものがタスク定義名
+  # ex. example:1
+  family = "example"
+  # タスクサイズ
+  # CPU Unitの整数表現(ex. 512, 1024)かvCPU(ex. 1 vCPU)も文字列表現で設定
+  cpu = "256"
+  # MiBの整数表現(ex. 1024)かGBの文字列表現(ex. 1 GB)
+  memory = "512"
+  # Fargate起動タイプではこちら
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  container_definitions    = file("./container_definitioins.json")
+}
+
+# ECSサービス
+resource "aws_ecs_service" "example" {
+  name            = "example"
+  cluster         = aws_ecs_cluster.example.arn
+  task_definition = aws_ecs_task_definition.example.arn
+  desired_count   = 2
+  launch_type     = "FARGATE"
+  # 明示的に指定しような
+  platform_version = "1.3.0"
+  # ヘルスチェック猶予期間
+  health_check_grace_period_seconds = 60
+
+  network_configuration {
+    assign_public_ip = false
+    security_groups  = [module.nginx_sg.security_group_id]
+
+    subnets = [
+      aws_subnet.private_0.id,
+      aws_subnet.private_1.id,
+    ]
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.example.arn
+    container_name   = "example"
+    container_port   = 80
+
+  }
+
+  lifecycle {
+    ignore_changes = [task_definition]
+  }
+}
+
+module "nginx_sg" {
+  source      = "./security_group"
+  name        = "nginx-sg"
+  vpc_id      = aws_vpc.example.id
+  port        = 80
+  cidr_blocks = [aws_vpc.example.cidr_block]
+}
